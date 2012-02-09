64a41004a799fa03c4991375608abe907fb58cc1
anhaa bold <anherdene@gmail.com>
2012-02-10 03:09:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JapanpageShare::Application.config.secret_token = 'aa4ab0632b86b2cdba6a4cd348c95df44ab7524052b4b25063ee297300985d4f2efef4cb67f54956f2b48891c98d6c5187cb581da33b1a6a20b8c2abf582d4c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JapanpageShare::Application.config.session_store :cookie_store, key: '_japanpage_share_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JapanpageShare::Application.config.session_store :active_record_store
